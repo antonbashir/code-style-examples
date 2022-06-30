33017e565a742779c6522a9d3f321bc382047aaa
Anton Bashirov <anton.bashirov@rt.ru>
2022-06-30 19:32:07 +0300

    examples

--- lib/examples/functions/functions.dart
+++ lib/examples/functions/functions.dart
@@ -1,21 +1,32 @@
 class FunctionsExample {
   void excellentFunction() {
-    // Хороший код тот, которого нет)) 
+    // Хороший код тот, которого нет))
   }
 
-  void bestFunction() {
+  void bestFunction(List<int> array) =>
+      array.map((element) => element + 1).toList();
 
+  void goodFunction(List<int> array) {
+    for (int index = 0; index < array.length; index++) {
+      array[index] = array[index] + 1;
+    }
   }
 
-  void goodFunction() {
-
-  }
-
-  void badFunction() {
-
+  void badFunction(List<int> array) {
+    array[0] = array[0] + 1;
+    array[1] = array[1] + 1;
+    array[2] = array[2] + 1;
+    array[3] = array[3] + 1;
+    array[4] = array[4] + 1;
+    array[5] = array[5] + 1;
+    array[6] = array[6] + 1;
+    array[7] = array[7] + 1;
+    array[8] = array[8] + 1;
+    array[9] = array[9] + 1;
+    array[10] = array[10] + 1;
   }
 
   void killMePlease() {
-
+    // Уберите детей от экрана: https://github.com/EpicGames/UnrealEngine/blob/release/Engine/Source/Runtime/Engine/Private/UnrealEngine.cpp
   }
 }
